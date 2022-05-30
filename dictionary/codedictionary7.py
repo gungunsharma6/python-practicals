fc446169081c21a84a9522baa95543a0b5442265
gungunsharma6 <106269920+gungunsharma6@users.noreply.github.com>
2022-05-30 18:42:30 +0530

    Update codedictionary7.py

--- dictionary/codedictionary7.py
+++ dictionary/codedictionary7.py
@@ -1,4 +1,4 @@
-#Write a program to get the maximum and minimum value of dictionary?
+#Write a program to get the maximum and minimum value of dictionary.
 dict={'key 1':100,'key 2':300}
 val=dict.values()
 max=max(val)
