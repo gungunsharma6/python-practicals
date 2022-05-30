27813c9d403a122881f97086f5be74114ef9ab7c
gungunsharma6 <106269920+gungunsharma6@users.noreply.github.com>
2022-05-30 18:40:31 +0530

    Update codedictionary2.py

--- dictionary/codedictionary2.py
+++ dictionary/codedictionary2.py
@@ -1,4 +1,4 @@
-#Write a program to iterate over dictionary items using for loop?
+#Write a program to iterate over dictionary items using for loop.
 dict={1:"Value1",2:"Value2",3:"Value3"}
 
 for key,val in dict.items():
