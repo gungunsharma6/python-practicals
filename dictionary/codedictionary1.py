85e817549a0ba7a689d32c2a2bc84d0cb1b386ca
gungunsharma6 <106269920+gungunsharma6@users.noreply.github.com>
2022-05-30 18:39:16 +0530

    Update codedictionary1.py

--- dictionary/codedictionary1.py
+++ dictionary/codedictionary1.py
@@ -1,4 +1,4 @@
-#Write a program to check whether a given key exists in a dictionary or not?
+#Write a program to check whether a given key exists in a dictionary or not.
 dic={'1':1,'2':2,'3':3}
 x=input()
 if x in dic.keys():
