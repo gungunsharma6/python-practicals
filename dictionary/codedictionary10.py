ae7890908ce54f28ff2f8491b0fc6d8f62d37cf9
gungunsharma6 <106269920+gungunsharma6@users.noreply.github.com>
2022-05-30 18:40:09 +0530

    Update codedictionary10.py

--- dictionary/codedictionary10.py
+++ dictionary/codedictionary10.py
@@ -1,4 +1,4 @@
-#Write a program to sort dictionary values in python?
+#Write a program to sort dictionary values in python.
 dict={'key1':"Apple",'key':"Mango",'key3':"Banana"}
 for key in sorted(dict):
     print("%s:%s"%(key,dict[key]))
