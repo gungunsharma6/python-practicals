225aa3818f2877df7aa39bdbb2b615c5a8b6c471
gungunsharma6 <106269920+gungunsharma6@users.noreply.github.com>
2022-05-30 18:50:18 +0530

    Update codefunction7.py

--- function/codefunction7.py
+++ function/codefunction7.py
@@ -1,4 +1,4 @@
-#Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters?
+#Write a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 def string_test(s):
     d={"UPPER_CASE":0, "LOWER_CASE":0}
     for c in s:
