881622a792e5cf419de3f2135e28344a29ffe978
gungunsharma6 <106269920+gungunsharma6@users.noreply.github.com>
2022-05-30 18:50:51 +0530

    Update codefunction8.py

--- function/codefunction8.py
+++ function/codefunction8.py
@@ -1,4 +1,4 @@
-#Write a Python function that takes a list and returns a new list with unique elements of the first list?
+#Write a Python function that takes a list and returns a new list with unique elements of the first list.
 def unique_list(l):
   x = []
   for a in l:
